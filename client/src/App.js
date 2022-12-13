a5ac4e1812e71ecaa37c0818a4c1a34745c72b70
Eriuden <antoinecarrette@hotmail.fr>
2022-12-13 08:55:02 +0100

    initialize

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+//Pour l'appel d'API, procéder autrement et checker eriu coaching pro
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
