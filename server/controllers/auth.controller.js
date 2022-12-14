38d9dd2ef0177834bbc98a5f1f5e352aa64ac53f
Eriuden <antoinecarrette@hotmail.fr>
2022-12-14 14:28:23 +0100

    auth

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,44 @@
+const userModel = require("../models/user.model")
+const jwt = require("jsonwebtoken")
+const {signInErrors, signUpErrors} = require("../utils/errors.utils")
+
+const maxAge = 3*24*60*60*1000
+
+const createToken = (id) => {
+    return jwt/sign({id}), process.env.TOKEN_SECRET, {
+        expiresIn: maxAge
+    }
+}
+
+module.exports.signUp = async (req, res) => {
+    const {name, email, password} = req.body
+
+    try {
+        const user = await userModel.create({name, email, password})
+        res.status(201).json({user: user._id})
+    }
+    catch(err){
+        const errors = signUpErrors(err)
+        res.status(200).send({errors})
+    }
+}
+
+module.exports.signIn = async (req,res) => {
+    const {email, password} = req.body 
+
+    try {
+        const user = await userModel.login(email, password)
+        const token = createToken(user._id)
+        res.cookie("jwt", token, { httpOnly: true, maxAge})
+        res.status(200).json({user: user._id})
+    }
+    catch(err) {
+        const errors = signInErrors(err)
+        res.status(200).json({errors})
+    }
+}
+
+module.exports.logout = (req,res) => {
+    res.cookie("jwt", "", {maxAge: 1})
+    res.redirect("/")
+}
---
+++ server/utils/errors.utils.js
@@ -0,0 +1,43 @@
+module.exports.signUpErrors = (err) => {
+    let errors = { name:"", email:"", password:"Mauvais mot de passe"}
+
+    if (err.message.includes("name"))
+    errors.name = "Pseudo incorrect ou déjà pris"
+
+    if (err.message.includes("email"))
+    errors.email = "Mail incorrect"
+
+    if (err.message.includes("npassword"))
+    errors.password = "Mot de passe incorrect"
+
+    if (err.code === 11000 && Object.keys(err.keyValue)[0].includes("name"))
+        errors.name = "Ce pseudo est déjà pris"
+
+    if (err.code === 11000 && Object.keys(err.keyValue)[0].includes("email"))
+    errors.email = "Ce mail est déjà pris"
+
+    return errors
+}
+
+
+module.exports.signInErrors = (err) => {
+    let errors = { email:"", password:""}
+
+    if(err.message.includes("email"))
+        errors.email = "Email inconnu"
+    
+    if(err.message.includes("password"))
+        errors.email = "Mot de passe inconnu"
+}
+
+module.exports.uploadErrors = (err) => {
+    let errors = { format:"" , maxSize: ""}
+
+    if (err.message.includes("invalid file"))
+    errors.format = "format d'image incompatible"
+
+    if(err.message.includes("max size"))
+    errors.format = "taille maximale de fichier dépassés"
+
+    return errors
+}
