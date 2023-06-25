ea4b0f95463ec005b334cccec6604ac8b66922bb
Antoine Carrette <71089756+Eriuden@users.noreply.github.com>
2023-06-25 15:37:57 +0200

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ mongoose
 .connect(
     "mongodb+srv://"
     + process.env.DB_USER_PASS +
-    "@cluster0.iodcc.mongodb.net/MERN-carte-age"
+
 )
 .then(()=> console.log("connecté à MongoDB"))
 .catch((err) => console.log("échec de connexion", err))
